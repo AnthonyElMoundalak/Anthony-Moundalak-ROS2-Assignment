cc006592faa5fb000d5fd437b66daa27352411cf
Anthony El Moundalak <anthony.moundalak@net.usj.edu.lb>
2024-07-17 15:38:30 +0300

    Implement temperature publisher node

--- ros2ws/src/temperature_package/setup.py
+++ ros2ws/src/temperature_package/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'temperature_publisher = temperature_package.temperature_publisher_node:main',
         ],
     },
 )
